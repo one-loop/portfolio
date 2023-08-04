2fef0484fc7ca098235c1dc1adeef82b68c96fd8
Saad <78092430+one-loop@users.noreply.github.com>
2023-08-04 22:42:18 +0530

    Added social media link preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { Container } from '@/components/container';
-import Head from 'next/head'
 import { Header } from '@/components/sections/header';
 import { Footer } from '@/components/sections/footer';
 import './globals.css';
 import type { Metadata } from 'next';
+import Head from './head';
 
 
 export const metadata: Metadata = {
@@ -15,11 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="width=device-width, height=device-height, initial-scale=1"
-        />
-        <link rel="icon" type="image/png" href="macintosh-favicon.png" />
+        <Head />
       </head>
       <body className="w-[100vw] overflow-x-hidden">
         <div>
