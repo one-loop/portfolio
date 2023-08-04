228f7b328d29b52d9587b812bce00f9a0936b77d
Saad <78092430+one-loop@users.noreply.github.com>
2023-08-04 12:05:15 +0530

    fixed errors for deployment

--- components/room-model-optimized.tsx
+++ components/room-model-optimized.tsx
@@ -19,8 +19,9 @@ export const RoomModel = () => {
     test.animate();
 
     // initialize global variables
-    let camera = test.camera, scene = test.scene;
-    let model: THREE.Object3D<THREE.Event> | THREE.AnimationObjectGroup
+    let camera = test.camera, scene = test.scene, renderer = test.renderer;
+    let model: THREE.Group
+    // let model: THREE.Object3D<THREE.Event> | THREE.AnimationObjectGroup
     let mixer: THREE.AnimationMixer, mouse: THREE.Vector2, raycaster: THREE.Raycaster;
     let isHoveringOverMonitor: boolean, isHoveringOverAquarium: boolean, canZoomOut = false, canInteract = false;
     let savedPosition: THREE.Vector3, targetPosition: THREE.Vector3, currentPosition: THREE.Vector3;
@@ -50,38 +51,33 @@ export const RoomModel = () => {
       // let monitorScreen = model.getObjectByName('Monitor_Screen');
       // let aquariumGlass = model.getObjectByName('Aquarium_Glass001');
 
-      // const geometry = new THREE.BoxGeometry(1.5, 1.5, 0.7);
-      // const deskHoverObject = new THREE.Mesh( geometry ); 
-      // deskHoverObject.name = 'desk hover';
-      // deskHoverObject.material.visible = false;
-      // // model.add(deskHoverObject);
-      // deskHoverObject.position.set(-0.15, 0.5, -1.45);
-      // console.log(deskHoverObject);
-
       model.traverse((child => {
-        if (child.name === 'Aquarium_Glass001') {
-          // glass texture seems to add a lot of lag/dropped frames
-          // so might be better to not include
-          aquariumGlassMaterial = new THREE.MeshPhysicalMaterial({
-            // roughness: 0,
-            // color: '#80aeff',
-            // transmission: 1,
-            // opacity: 1,
-            visible: false
-          });
-          child.material = aquariumGlassMaterial;
-        } else if (child.name === 'Monitor_Screen' || child.name === 'Laptop_Screen') {
-          monitorScreenMaterial = new THREE.MeshBasicMaterial({ map: videoTexture2 });
-          monitorScreenMaterial.map.encoding = THREE.sRGBEncoding;
-          child.material = monitorScreenMaterial;
-        } else if (child.name === 'Computer_Screen') {
-          computerScreenMaterial = new THREE.MeshBasicMaterial({ map: videoTexture2 });
-          computerScreenMaterial.map.encoding = THREE.sRGBEncoding;
-          child.material = computerScreenMaterial;
+        if (child instanceof THREE.Mesh) {
+          if (child.name === 'Aquarium_Glass001') {
+            // glass texture seems to add a lot of lag/dropped frames
+            // so might be better to not include
+            aquariumGlassMaterial = new THREE.MeshPhysicalMaterial({
+              // roughness: 0,
+              // color: '#80aeff',
+              // transmission: 1,
+              // opacity: 1,
+              visible: false
+            });
+            child.material = aquariumGlassMaterial;
+          } else if (child.name === 'Monitor_Screen' || child.name === 'Laptop_Screen') {
+            monitorScreenMaterial = new THREE.MeshBasicMaterial({ map: videoTexture2 });
+            if (monitorScreenMaterial.map) monitorScreenMaterial.map.encoding = THREE.sRGBEncoding;
+            child.material = monitorScreenMaterial;
+          } else if (child.name === 'Computer_Screen') {
+            computerScreenMaterial = new THREE.MeshBasicMaterial({ map: videoTexture2 });
+            if (computerScreenMaterial.map) computerScreenMaterial.map.encoding = THREE.sRGBEncoding;
+            child.material = computerScreenMaterial;
+          }
         }
         
+        
         // can't apply shadows to ambient or directional lights
-        if (child.isObject3D && !child.isLight) {
+        if (child.isObject3D && !(child instanceof THREE.Light)) {
           child.castShadow = true;
           child.receiveShadow = true;
         }
@@ -104,21 +100,14 @@ export const RoomModel = () => {
         mouse.y = -((event.clientY - rect.top) / (rect.height)) * 2 + 1;
 
         // update raycaster with the new mouse position
-        raycaster.setFromCamera(mouse, camera);
+        raycaster.setFromCamera(mouse, camera!);
 
         // Intersects objects in the scene with the raycaster
         const intersects = raycaster.intersectObjects(model.children);
 
-        // if (intersects.length) {
-        //   console.log(intersects[0].object.name)
-        // }
+        // if (intersects.length) console.log(intersects[0].object.name)
 
         // if (intersects.length > 0 && deskItems.includes(intersects[0].object.name)) {
-        //   isHoveringOverMonitor = true;
-        //   // console.log("hovering over monitor screen!");
-        //   canvasContainer.style.cursor = 'pointer';
-        // } 
-        // console.log(intersects[0].object.name);
         if (intersects.length > 0 && deskItems.includes(intersects[0].object.name)) {
           isHoveringOverMonitor = true;
           // console.log("hovering over monitor screen!");
@@ -150,7 +139,7 @@ export const RoomModel = () => {
             duration: 1,
             ease: 'power2.out',
             onUpdate: () => {
-              camera.position.copy(currentPosition);
+              camera!.position.copy(currentPosition);
             },
             onComplete: () => {
               // This callback is executed after the camera movement is complete
@@ -159,7 +148,7 @@ export const RoomModel = () => {
             },
           });
         } else if (isHoveringOverMonitor) {
-          savedPosition = camera.position.clone();
+          savedPosition = camera!.position.clone();
           targetPosition = new THREE.Vector3(-0.15, 0.5, -1.45);
           // Start the GSAP animation to move the camera to the target position
           gsap.to(savedPosition, {
@@ -169,14 +158,14 @@ export const RoomModel = () => {
             duration: 1,
             ease: 'power2.out',
             onUpdate: () => { // runs every frame
-              camera.position.copy(savedPosition);
+              camera!.position.copy(savedPosition);
             },
             onComplete: () => {
               canZoomOut = true;
             },
           });
         } else if (isHoveringOverAquarium) {
-          savedPosition = camera.position.clone();
+          savedPosition = camera!.position.clone();
           targetPosition = new THREE.Vector3(2.0, 0.25, 0.5);
           
           gsap.to(savedPosition, {
@@ -186,7 +175,7 @@ export const RoomModel = () => {
             duration: 1,
             ease: 'power2.out',
             onUpdate: () => { // runs every frame
-              camera.position.copy(savedPosition);
+              camera!.position.copy(savedPosition);
             },
             onComplete: () => {
               canZoomOut = true;
@@ -200,9 +189,9 @@ export const RoomModel = () => {
         mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	      mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-        model.rotation.y = mouse.x * 0.3;
-        model.rotation.z = mouse.y * 0.1;
-        model.rotation.x = mouse.y * -0.1;
+        scene!.rotation.y = mouse.x * 0.3;
+        scene!.rotation.z = mouse.y * 0.1;
+        scene!.rotation.x = mouse.y * -0.1;
       }
       
       const ambientLight = new THREE.AmbientLight(0xC18E5A, 0.05);
@@ -275,7 +264,7 @@ export const RoomModel = () => {
       const clickObject = document.querySelector('#click-object') as HTMLButtonElement;
       clickObject.addEventListener('click', () => {
         if (!canZoomOut) {
-          savedPosition = camera.position.clone();
+          savedPosition = camera!.position.clone();
           targetPosition = new THREE.Vector3(-0.15, 0.5, -1.45);
           // Start the GSAP animation to move the camera to the target position
           gsap.to(savedPosition, {
@@ -285,7 +274,7 @@ export const RoomModel = () => {
             duration: 1,
             ease: 'power2.out',
             onUpdate: () => { // runs every frame
-              camera.position.copy(savedPosition);
+              camera!.position.copy(savedPosition);
             },
             onComplete: () => {
               canZoomOut = true;
@@ -294,9 +283,9 @@ export const RoomModel = () => {
         }
       })
       
-      scene.position.y = -1;
-      camera.position.set(6.8, 4.5, 8.0)
-      scene.add(model);
+      scene!.position.y = -1;
+      camera!.position.set(6.8, 4.5, 8.0)
+      scene!.add(model);
 
       // to play animations
       mixer = new THREE.AnimationMixer(model);
@@ -307,17 +296,12 @@ export const RoomModel = () => {
       const clock = new THREE.Clock();
       const animate = () => {
         // anything in here gets called every frame
-        // if (model) {
-        //   model.rotation.x += 0.01;
-        //   model.rotation.y += 0.01;
-        //   model.rotation.z += 0.01;
-        // }
         // labelRenderer.render(scene, camera);
         // TWEEN.update();
-        // test.renderer.render( scene, camera );
+        // v prevents jittering animations but seems to add lag to the scene (30 fps drop)
+        // renderer!.render( scene!, camera! ); 
         requestAnimationFrame(animate);
         mixer.update(clock.getDelta());
-
       };
       animate();
       setLoading(false);
@@ -331,11 +315,11 @@ export const RoomModel = () => {
       {isLoading && ( // Show loading message or spinner while isLoading is true
         <>
           <div aria-label="Default" className="relative inline-flex flex-col gap-2 items-center justify-center">
-            <div className="relative flex w-8 h-8">
-              <i className="absolute w-full h-full rounded-full animate-spinner-ease-spin [border:solid_3px_#8ccbff] !border-t-transparent !border-l-transparent !border-r-transparent"></i>
-              <i className="absolute w-full h-full rounded-full opacity-75 animate-spinner-linear-spin [border:dotted_3px_#8ccbff] !border-t-transparent !border-l-transparent !border-r-transparent"></i>
+            <div className="relative flex w-10 h-10">
+              <i className="absolute w-full h-full rounded-full animate-spinner-ease-spin [border:solid_3px_#006FEE] !border-t-transparent !border-l-transparent !border-r-transparent"></i>
+              <i className="absolute w-full h-full rounded-full opacity-75 animate-spinner-linear-spin [border:dotted_3px_#006FEE] !border-t-transparent !border-l-transparent !border-r-transparent"></i>
             </div>
-            <span className="text-md text-[#8ccbff]">Loading...</span>
+            <span className="text-md text-off-white">Loading...</span>
           </div>
         </>
       )}
@@ -345,5 +329,3 @@ export const RoomModel = () => {
   )
 
 }
-
-
