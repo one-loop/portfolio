b712da4c26a03052348fbd9d25b073d0b659b72e
Saad <78092430+one-loop@users.noreply.github.com>
2023-08-04 15:39:13 +0530

    Update footer.tsx

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -7,7 +7,7 @@ import { MailLogo } from "../logos/mail";
 export const Footer = () => (
   <footer className="text-md text-primary-text text-center py-10 border-t border-transparent-white">
     <p>
-      <Link href="https://raw.githubusercontent.com/one-loop/portfolio/main/components/humans.txt" className="text-off-white hover:text-primary-text [transition:color_0.2s] text-sm">Developed by Saad Sifar</Link>
+      <Link href="https://raw.githubusercontent.com/one-loop/portfolio/main/components/humans.txt" target="_blank" className="text-off-white hover:text-primary-text [transition:color_0.2s] text-sm">Developed by Saad Sifar</Link>
     </p>
     <div className="fill-primary-text [&_svg]:w-[2rem] [&_svg]:inline [&_svg]:mr-4">
           <Link href="https://www.linkedin.com/in/saad-sifar" target="_blank" className="hover:fill-off-white [transition:fill_0.2s_ease]"><LinkedinLogo/></Link>
